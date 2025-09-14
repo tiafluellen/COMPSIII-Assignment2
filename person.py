b86d09f5ee5da81a65965a82032f4e5d89602ba9
tiafluellen <tiafluellen@abbey.bac.edu>
2025-09-14 17:20:34 -0400

    Update person.py

--- person.py
+++ person.py
@@ -1,3 +1,11 @@
 class Person:
-    # Delete pass and write your code here
-    pass
+    def __init__(self, name, age, country):
+        self.name = name
+        self.age = age
+        self.country = country
+
+    def __str__(self):
+        return f"{self.name} is {self.age} years old and is from {self.country}."
+
+    def can_vote(self):
+        return self.age >= 18
